5b5ee94e740ff3797a87536a796bfd0dd498c8df
Lydia MESSAOUDENE <78177457+lydia385@users.noreply.github.com>
2024-06-07 13:15:13 +0100

    torch done

--- causaleffect.py
+++ causaleffect.py
@@ -47,7 +47,7 @@ def joint_uncond(params, decoder, classifier, adj, feat, node_idx=None, act=torc
     zs = torch.cat([alpha, beta], dim=-1)  
     xhat = act(decoder(zs)) * adj
     if brain:
-        logits=np.array([])
+        logits=np.array()
 
         for i in range(xhat.shape[0]):
             x_hat_i=xhat[i]
@@ -56,7 +56,7 @@ def joint_uncond(params, decoder, classifier, adj, feat, node_idx=None, act=torc
                 # print(f" index : {i} ")
             else:
                 logits_i = classifier(feat, x_hat_i)[0][:,node_idx,:]
-            logits = np.append(logits,torch.Tensor(logits_i), axis=0)
+            logits = np.append(logits,logits_i, axis=0)
     else:
         if node_idx is None:
             logits = classifier(feat_new, xhat)[0]
